0a7b834f1420a3f867fde86b56c0b7b85fec76a1
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-29 14:01:11 +0600

    frontend updated

--- frontend/script.js
+++ frontend/script.js
@@ -47,7 +47,7 @@ document.getElementById('predictionForm').addEventListener('submit', async (e) =
     console.log("Sending data:", formData); // Debug log
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
+        const response = await fetch('https://failure-risk-prediction-57et.onrender.com/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
